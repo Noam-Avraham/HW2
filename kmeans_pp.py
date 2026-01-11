b7f77ee216a1ca271e1cc395b4f60b486727bfe8
noam avraham <noamavraham@mail.tau.ac.il>
2026-01-11 12:32:24 +0200

    data read and merge

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -1,8 +1,9 @@
 import numpy as np
-inport sys
+import pandas as pd
+import sys
  
- def main()
- #input parsing
+def main():
+ #input parsing k is clusters number, max_iter is maximum iterations, eps is epsilon for convergence, input_file1 and input_file2 are input files.
     args = sys.argv
     if len(args)>6 or len(args)<5:
         print("An Error Has Occurred")
@@ -12,13 +13,19 @@ inport sys
     except ValueError:
         print("Incorrect number of clusters!")
         sys.exit(1)
+    if k<=0:
+        print("Incorrect number of clusters!")
+        sys.exit(1)
+
     if(len(args)<6):
-        has_iter=0
+        max_iter=300
     else:
-        has_iter=1
         try:
             max_iter=int(args[2])
         except ValueError:
+            print("Incorrect number of iterations!")
+            sys.exit(1)
+        if max_iter<=0 or max_iter>=800:
             print("Incorrect number of iterations!")
             sys.exit(1)
     try:
@@ -26,11 +33,22 @@ inport sys
     except ValueError:
         print("Incorrect epsilon!")
         sys.exit(1)
+    if eps<0:
+        print("Incorrect epsilon!")
+        sys.exit(1)
     # we can assume validity of input files.
     input_file1=args[-2]
     input_file2=args[-1]
 #input merging
+    
+    data1 = pd.read_csv(input_file1, header=None)
+    data2 = pd.read_csv(input_file2, header=None)
 
+    data = pd.merge(data1, data2, on=0, how='inner')
+    data = data.sort_values(by=0)
+    data = data.drop(columns=[0])
+
+    pd.to_csv(data, 'merged_data.csv', header=False, index=False)
 
 if __name__ == "__main__":
     main()
