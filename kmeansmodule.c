e289593597cdb0ff172b894b3d426b34d62a4e34
noam avraham <noamavraham@mail.tau.ac.il>
2026-01-20 13:02:45 +0200

    fixing var declerations

--- kmeansmodule.c
+++ kmeansmodule.c
@@ -5,32 +5,38 @@
 
 double distance(double* point1, double* point2, int dim) {
     double sum = 0.0;
+    double diff;
     for(int i = 0; i < dim; i++) {
-        double diff = point1[i] - point2[i];
+        diff = point1[i] - point2[i];
         sum += diff * diff;
     }
     return sqrt(sum);
 }
 double add_vectors(double* vec1, double* vec2, int dim,int opertion) {
-    for(int i = 0; i < dim; i++) {
+    int i;
+    for(i = 0; i < dim; i++) {
         vec1[i] += vec2[i]*opertion;
     }
     return 0;
 }
 double copy_vector(double* src, int dim) {
-    double* dest=(double*)malloc(dim * sizeof(double));
-    for(int i = 0; i < dim; i++) {
+    int i;
+    double* dest;
+    dest=(double*)malloc(dim * sizeof(double));
+    for(i = 0; i < dim; i++) {
         dest[i] = src[i];
     }   
     return dest;
 }
 void update_clusters(double** points, double** clusters, int* points_to_cluster,double** clusters_sums,int* clusters_counts, int k, int dim, int n) {
     /** reassign the first point to empty cluster */
-    for (int i = 0; i < k; i++) {
+    int i,j;
+    int previous_index
+    for (i = 0; i < k; i++) {
         clusters_counts[i] = 0;
-        int previous_index= points_to_cluster[0];
+        previous_index= points_to_cluster[0];
         points_to_cluster[0]=i;
-        for(int j = 0; j < dim; j++) {
+        for(j = 0; j < dim; j++) {
             clusters_sums[previous_index][j]-= points[0][j];
             clusters_sums[i][j]+=points[0][j];
         }
@@ -43,40 +49,44 @@ static PyObject* fit(PyObject* self, PyObject* args) {
     PyObject *clusters_obj ,*points_obj, *points_to_cluster_obj;
     int k, max_iter,n,dim;
     double eps;
-
+    int i,j;
+    int* points_to_cluster;
+    double** clusters_sums;
+    points_to_cluster = (int*)malloc(n * sizeof(int));
+    clusters_sums = (double**)malloc(k * sizeof(double*));
     if(!PyArg_ParseTuple(args, "OOOiiiid", &points_obj, &clusters_obj,&points_to_cluster_obj, &k, &max_iter, &dim,&n, &eps)) {
         return NULL;
     }
     
     /**initalize points to python.points */
     double** points = (double**)malloc(n * sizeof(double*));
-    for(int i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) {
         points[i] = (double*)malloc(dim * sizeof(double));
         PyObject* point = PyList_GetItem(points_obj, i);
-        for(int j = 0; j < dim; j++) {
+        for(j = 0; j < dim; j++) {
             PyObject* coord =  PyList_GetItem(point, j);
             points[i][j] = PyFloat_AsDouble(coord);
         }
     }
     /**initalize clusters to python.clusters */
     double** clusters = (double**)malloc(k * sizeof(double*));
-    for(int i = 0; i < k; i++) {
+    for(i = 0; i < k; i++) {
         clusters[i] = (double*)malloc(dim * sizeof(double));
         PyObject* cluster = PyList_GetItem(clusters_obj, i);
-        for(int j = 0; j < dim; j++) {
+        for(j = 0; j < dim; j++) {
             PyObject* coord = PyList_GetItem(cluster, j);
             clusters[i][j] = PyFloat_AsDouble(coord);
         }
     }
     /**initalize points_to_cluster to python.points_to_cluster */
-    int* points_to_cluster = (int*)malloc(n * sizeof(int));
-    for(int i = 0; i < n; i++) {
+   
+    for(i = 0; i < n; i++) {
         PyObject* cluster_index = PyList_GetItem(points_to_cluster_obj, i);
         points_to_cluster[i] = PyLong_AsLong(cluster_index);
     }
     /**initalize clusters_sums and clusters_counts */
-    double** clusters_sums = (double**)malloc(k * sizeof(double*));
-    for (int i = 0; i < k; i++) {
+    
+    for (i = 0; i < k; i++) {
         clusters_sums[i] = (double*)malloc(dim * sizeof(double));
     }
     int* clusters_counts = (int*)malloc(k * sizeof(int));
